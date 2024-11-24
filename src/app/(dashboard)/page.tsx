c56f5cc69c4fec1e8d567a7868bb5ecb77456030
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-11-23 22:35:16 -0800

    New breakign branch

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,15 +4,6 @@ import {
   AcceptedFriendsList,
   PendingFriendsList,
 } from "./_components/friends-list";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { AddFriend } from "./_components/add-friend";
-
-export default function FriendsPage() {
-  return (
-    <div className="flex-1 flex-col flex divide-y">
-      <header className="flex items-center justify-between p-4">
-        {/* Code comment */}
-        {/* other suggestions *\
         </TooltipProvider>
       </div>
     </div>
