934a4ef96f43d26d38c9d7af873e5ae812091e78
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-11-24 18:01:19 -0800

    Another

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,8 +16,6 @@ export default function DashboardLayout({
           <DashboardSidebar />
           {children}
         </SidebarProvider>
-      <Unauthenticated>
-        <RedirectToSignIn />
       </Unauthenticated>
     </>
   );
